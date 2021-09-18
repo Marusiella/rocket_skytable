92cb9e67f7a3b45e76fc851d769f9878dfb38773
Marusiella <67511608+Marusiella@users.noreply.github.com>
2021-09-18 13:54:44 +0200

    clear code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use rocket::response::content::Html;
 use skytable::actions::Actions;
 use skytable::Connection;
 use skytable::ConnectionBuilder;
-use rand::Rng;
 use urlencoding::decode;
 use rocket::{Build, Rocket};
 
@@ -14,20 +13,6 @@ const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz\
 const PASSWORD_LEN: usize = 4;
 
 
-#[get("/api/set/<x>")]
-fn index(x:&str) -> String {
-    use rand::prelude::*;
-    let mut thread_rng = thread_rng();
-    let link: String = (0..PASSWORD_LEN)
-        .map(|_| {
-            let idx = thread_rng.gen_range(0..CHARSET.len());
-            CHARSET[idx] as char
-        })
-        .collect();
-    let mut con = Connection::new("127.0.0.1", 2003).unwrap();
-    con.set(&link, x).unwrap();
-    link
-}
 
 #[get("/<x>")]
 fn get(x:&str) -> Html<String> {
@@ -36,7 +21,6 @@ fn get(x:&str) -> Html<String> {
         Ok(t) => t,
         Err(_) => format!("null"),
     };
-    // decode(&key).unwrap().to_string()
     Html(format!(r#"<script type="text/javascript">
             window.location.href = "{}"
         </script>"#,decode(&key).unwrap().to_string()))
@@ -59,28 +43,9 @@ fn test(input: String) -> Html<String> {
         .set_entity("default:default".to_owned())
         .get_connection()
         .unwrap();
-    // con.set(&link, &input).unwrap();
-    // con.update(&link, input).unwrap();
     con.uset([link.clone()], [input]).unwrap();
     Html(format!(r#"<a href="http://localhost:8000/{}">http://localhost:8000/{}</a>"#,link,link))
-    // Html(format!(r#"<a href="http://localhost:8000/{}">http://localhost:8000/{}</a>
-    // <script type="text/javascript">
-    //   function isValidHttpUrl(string) {{
-    //     let url;
-    //
-    //     try {{
-    //       url = new URL(string);
-    //     }} catch (_) {{
-    //       return false;
-    //     }}
-    //
-    //     return true;
-    //   }}
-    //   if (isValidHttpUrl("http://www.google.com/")) {{
-    //       document.write("")
-    //   }}
-    // </script>
-    // "#,link,link))
+
 }
 #[get("/")]
 fn wew() -> Html<&'static str> {
@@ -110,5 +75,5 @@ fn null() -> Html<&'static str> {
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![index,get,test,wew,null])
+    rocket::build().mount("/", routes![get,test,wew,null])
 }
